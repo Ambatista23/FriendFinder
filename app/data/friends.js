763942203528b109cf748d278f2c55fa0c902e32
Andre Batista <andrebatista@andres-air.home>
2019-07-20 21:31:40 -0400

    debug deployment issue

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+  {
+    name: "John",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+  },
+  {
+    name: "Phil",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 4, 3, 1, 1, 1, 2, 2, 4, 5]
+  },
+  {
+    name: "Lisa",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 4, 3, 2, 1, 4, 2, 3, 4, 5]
+  },
+  {
+    name: "Tim",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 4, 3, 2, 5, 1, 2, 3, 4, 5]
+  },
+  {
+    name: "Maria",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 4, 2, 2, 1, 1, 3, 3, 4, 5]
+  }
+];
+
+module.exports = friends;
